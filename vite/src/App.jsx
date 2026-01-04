2d48ffcee3dbea649b71cd12da66aea9a4defeb9
dwalczak97 <d.melon@op.pl>
2026-01-04 07:07:35 +0100

    navlink

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/HomePage.jsx';
 import Movie from './pages/MoviesPage.jsx';
 import NotFound from './pages/Not found.jsx';
+import Navigation from './components/Navigation.jsx';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <>
+    <Navigation/>
 <Routes>
   <Route path='/' element={<Home />}></Route>
   <Route path='/movie' element={<Movie />}></Route>
--- vite/src/components/Navigation.jsx
+++ vite/src/components/Navigation.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navigation = () => {
-
+return(
+    <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movie">Movie</NavLink>
+    </nav>
+)
 };
 
 export default Navigation;
